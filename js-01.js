68eb72dd1cd8090c919a4729c4fdad33ed239a4e
Viktoria <viktoria.ttravel@gmail.com>
2023-04-01 11:58:58 +0300

    3, 4 task

--- js-01.js
+++ js-01.js
@@ -19,4 +19,48 @@
 // } else if (a >= 30 && a < 45) {
 //     console.log("Третя чверть")
 // } else { console.log("Четверта чверть") }
-    
+
+//TODO:===================================
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+  
+// const num = 5;
+// let result = "";
+
+// switch (num) {
+//     case 1:
+//         result = "Winter"
+//         break;
+//     case 2:
+//         result = "Spring"
+//         break;
+//     case 3:
+//         result = "Summer"
+//         break;
+//     case 4:
+//         result = "Autumn"
+//         break;
+
+//     default:
+//         result = "Error"
+//         break;
+// }
+// console.log(result)
+
+//TODO:===================================
+//Використовуючи конструкцію if...else,
+//напишіть код, який запитуватиме:
+//"Яка офіційна назва JavaScript?"
+//Якщо користувач вводить "ECMAScript",
+// показати через alert: "Вірно!"
+//інакше відобразити: "Не знаєте? ECMAScript!"
+
+// const result = prompt("Яка офіційна назва JavaScript?")
+// console.log(result);
+
+// if (result === "ECMAScript") {
+//     alert("Вірно!")
+// } else {
+//     alert("Не знаєте? ECMAScript!")
+// }
