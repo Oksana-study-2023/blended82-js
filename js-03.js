30cb4b62f06bd0a195aedb982df3b5ec31a72b25
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-04-29 10:44:04 +0200

    calc

--- js-03.js
+++ js-03.js
@@ -31,3 +31,49 @@
 // console.log(firstClient.client);
 // firstClient.email = 'cat@gmail.com';
 // console.log(firstClient.client);
+
+//TODO:======================
+// Створи клас для калькулятора, який має такі методи:
+// метод number, який набуває початкового значення для наступних операцій
+// Геттер, який повертає фінальний результат усіх операцій, проведених із числом
+// методи add, substruct, divide, multiply
+
+// class Calculator {
+//     constructor(){
+//         this.number = 0;
+//     }
+
+//     get resultCalc(){
+//         return this.number;
+//     }    
+
+//     addCalc(number){
+//         return this.number = this.number + number;
+//     }
+
+//     substruct(number){
+//         return this.number -= number;
+//     }
+
+//     divide(number){
+//         if (number !== 0){
+//             return this.number /= number;
+//         }
+//     }
+
+//     multiply(number){
+//         return this.number *= number;
+//     }
+
+// }
+// const calc = new Calculator();
+// // console.log(calc.addCalc(5));
+// // console.log(calc.addCalc(10));
+// // console.log(calc.substruct(7));
+
+// calc.addCalc(5);
+// calc.addCalc(10);
+// calc.substruct(7);
+// calc.divide(-2);
+// calc.multiply(3);
+// console.log(calc.resultCalc);
